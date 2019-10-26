d333b4ca4a03dc771538a7e85ced42eec0e2e699
Henning Odén <henning.oden@outlook.com>
2019-10-26 12:08:51 +0200

    Fixed Rotate. Final step to produce correct result still remains.

--- KattisDRM/DRMEncryptor.cs
+++ KattisDRM/DRMEncryptor.cs
@@ -26,18 +26,21 @@ namespace KattisDRM
             
         }
 
+        // Divides a string of an even count of uppercase letters only in half.
         public void Divide(string str)
         {
             firstHalf = str.Substring(0, str.Length / 2);
             lastHalf = str.Substring(str.Length / 2);
         }
 
+        // Overload for Rotate of one string
         public string Rotate(string oldStr)
         {
             var rotateString = GetRotateValue(oldStr);
             return Rotate(oldStr, rotateString);
         }
 
+        // Processes a string to generate a string of a series of identical values to rotate a string with by itself for the Rotate step of DRM encryption
         private static string GetRotateValue(string oldStr)
         {
             int rotValue = 0;
@@ -53,13 +56,14 @@ namespace KattisDRM
             return new string(rotateBase);
         }
 
+        // Performs actual rotation of a string by the characters in the second argument. Will double for Merge due to implementation.
         public string Rotate(string str, string rotateString)
         {
             char[] characters = new char[str.Length];
             for (int i = 0; i < str.Length; i++)
             {
                 int charValue = Convert.ToInt32(str[i]);
-                charValue += Convert.ToInt32(str[i]) - 65;
+                charValue += Convert.ToInt32(rotateString[i]) - 65;
                 while(charValue > 90)
                 {
                     charValue -= 26;
--- KattisDRMTest/UnitTest1.cs
+++ KattisDRMTest/UnitTest1.cs
@@ -6,21 +6,34 @@ namespace KattisDRMTest
 {
     public class UnitTest1
     {
+        DRMEncryptor encryptor = new DRMEncryptor();
         [Fact]
         public void RotateRotatesCorrectly()
         {
-            DRMEncryptor encryptor = new DRMEncryptor();
             var res = encryptor.Rotate("EWPG");
             Assert.Equal("ZRKB", res);
         }
 
         [Fact]
+        public void RotateCanMergeStrings()
+        {
+            var res = encryptor.Rotate("ZRKB", "BKSC");
+            Assert.Equal("ABCD", res);
+        }
+
+        [Fact]
         public void DivideDividesCorrectly()
         {
-            DRMEncryptor encryptor = new DRMEncryptor();
             encryptor.Divide("EWPGAJRB");
             Assert.Equal("EWPG", encryptor.firstHalf);
             Assert.Equal("AJRB", encryptor.lastHalf);
         } 
+
+        [Fact]
+        public void EncryptProducesCorrectResult()
+        {
+            var result = encryptor.Encrypt("EWPGAJRB");
+            Assert.Equal("ABCD", result);
+        }
     }
 }
